f7d23a63bee8a239600f37bb314d55e634b63b34
sela567 <ivan.selasb@gmail.com>
2026-03-24 20:56:38 +0100

    Update zad4.py

--- LV2_seletkovic/zad4.py
+++ LV2_seletkovic/zad4.py
@@ -1,6 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
-def sahovnica(velicina,broj,sirina)
-    bijelo=np.ones((velicina,velicina))*255
-    crno=np.zeros((velicina,velicina))
+def sahovnica(kvadrat,visina,sirina):
+    bijeli=np.ones((kvadrat,kvadrat))*255 
+    crni=np.zeros((kvadrat,kvadrat))  
+
+    redovi=[]
+    
+    for i in range(visina): 
+        red=[]
+        
+        for j in range(sirina):
+            if(i+j)%2==1:
+                red.append(bijeli) 
+            else:
+                red.append(crni) 
+        redovi.append(np.hstack(red))
+        
+    return np.vstack(redovi)
+
+img=sahovnica(50,4,5)
+plt.imshow(img,cmap='gray',vmin=0,vmax=255)
+plt.show()
